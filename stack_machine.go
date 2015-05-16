348468ea69edf480ac945d392d7b682af16497ee
Katherine Wallace <kwalla89@gmail.com>
2015-05-16 19:32:12 -0400

    Adds addition

--- stack_machine.go
+++ stack_machine.go
@@ -2,13 +2,56 @@ package main
 
 import(
 	"fmt"
+	"errors"
 )
 
 func main() {
-	whoAmI()
+	var sm StackMachine
+	sm.operators = map[rune]func()error{
+		'+': sm.add,
+	}
+	fmt.Println(sm.calculate("12+4++"))
 }
 
 func whoAmI() string {
-	fmt.Println("I'm a stack machine")
 	return "Stack Machine"
+}
+
+type StackMachine struct {
+	numStack []int
+	operators   map[rune]func()error
+}
+
+func (sm *StackMachine) pop() (num int, err error) {
+	if len(sm.numStack) < 1 {
+		err = errors.New("Not enough numbers")
+		return
+	}
+	num, sm.numStack = sm.numStack[len(sm.numStack)-1], sm.numStack[:len(sm.numStack)-1]
+	return
+}
+
+func (sm *StackMachine) calculate(userInput string) int {
+	for _, r := range userInput {
+		if sm.operators[r] != nil {
+			err := sm.operators[r]()
+			if err != nil{
+				return -1
+			}
+		} else {
+			sm.numStack = append(sm.numStack, int(r - '0'))
+		}
+	}
+	return sm.numStack[len(sm.numStack) - 1]
+}
+
+func (sm *StackMachine) add() (err error) {
+	var num1, num2 int
+	num1, err = sm.pop()
+	num2, err = sm.pop()
+	if err != nil {
+		return
+	}
+	sm.numStack = append(sm.numStack, num1 + num2)
+	return
 }
